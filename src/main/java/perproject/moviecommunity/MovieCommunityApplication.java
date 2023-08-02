0590b42dcf84b6cd5adf06275cf974951f07058c
YANGHAEUN <y210he@gmail.com>
2023-08-02 19:40:12 +0900

    first commit

---
+++ src/main/java/perproject/moviecommunity/MovieCommunityApplication.java
@@ -0,0 +1,13 @@
+package perproject.moviecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCommunityApplication.class, args);
+	}
+
+}
