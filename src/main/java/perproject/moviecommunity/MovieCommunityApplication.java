920b3182f4918ed86c503fff5c14a93e0436985b
YANGHAEUN <y210he@gmail.com>
2023-08-11 13:56:13 +0900

    0811 jar -> war modify

--- src/main/java/perproject/moviecommunity/MovieCommunityApplication.java
+++ src/main/java/perproject/moviecommunity/MovieCommunityApplication.java
@@ -2,9 +2,16 @@ package perproject.moviecommunity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovieCommunityApplication {
+public class MovieCommunityApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieCommunityApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCommunityApplication.class, args);
