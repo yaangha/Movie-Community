cdfed5737d76c51774e90b1e399a638d3d03d60b
YANGHAEUN <y210he@gmail.com>
2023-09-02 12:13:03 +0900

    회원가입시 길이 제한, 패턴 추가

--- src/main/java/perproject/moviecommunity/dto/MemberRegisterDto.java
+++ src/main/java/perproject/moviecommunity/dto/MemberRegisterDto.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Pattern;
 public class MemberRegisterDto {
 
     @NotBlank(message = "아이디는 필수 입력값입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{4,10}$", message = "아이디는 4~10자 영문과 숫자 조합만 가능합니다.")
+    @Pattern(regexp = "(?=.*[a-z0-9]).{4,10}$", message = "아이디는 4~10자 영문과 숫자 조합만 가능합니다.")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
